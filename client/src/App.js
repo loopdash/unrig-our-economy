f3b1894715c9abc9df7450873671ab3ecb371533
Merel Jacobs <merel.burleigh@gmail.com>
2025-02-19 10:43:36 -0800

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 // client/src/App.js
 import React, { useEffect, useState } from 'react';
-import { getKrogerProducts  } from './services/api';
+import { getKrogerProducts, getProductScrapings  } from './services/api';
 
 function App() {
     const [krogerProducts, setKrogerProducts] = useState([]);
+    const [productScraping, setProductScrapings] = useState([]);
 
     // Fetch products
     useEffect(() => {
         fetchKrogerProducts();
+        fetchProductScrapings();
     }, []);
 
     const fetchKrogerProducts = async () => {
@@ -19,14 +21,21 @@ function App() {
         }
     };
 
-   
+    const fetchProductScrapings = async () => {
+      try {
+          const data = await getProductScrapings();
+          setProductScrapings(data);
+      } catch (error) {
+          console.error('Failed to fetch products:', error);
+      }
+  };
     return (
         <div>
-            <h1>Product List</h1>
+            <h1>Kroger Product List</h1>
             <ul>
-                {krogerProducts.map(product => (
+                {productScraping.map(product => (
                     <li key={product.id}>
-                        {product.name} - ${product.price}
+                        {product.product_name} - ${product.product_price}
                     </li>
                 ))}
             </ul>
--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    host: '159.89.39.181',
+    user: 'reactuser',   // <-- Hardcoded for testing
+    password: 'ReactUser@1234',  // <-- Hardcoded for testing
+    database: 'unrigDatabase',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
