f42b330dcac65bf924ac15e336196629b1301695
Merel Jacobs <merel.burleigh@gmail.com>
2025-02-19 13:30:32 -0800

    updates

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // const API_URL = 'http://localhost:5001/';
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001/';
 
-
+console.log('API URL', API_URL)
 // Get all Kroger products
 export const getKrogerProducts = async () => {
     try {
