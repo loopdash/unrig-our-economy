af93cd9b56a7967904a47740cd596f4bc07ab3a7
Merel Jacobs <merel.burleigh@gmail.com>
2025-02-19 10:43:48 -0800

    updates

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 
 const db = mysql.createPool({
     host: '159.89.39.181',
-    user: 'reactuser',   // <-- Hardcoded for testing
-    password: 'ReactUser@1234',  // <-- Hardcoded for testing
-    database: 'unrigDatabase',
-    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
