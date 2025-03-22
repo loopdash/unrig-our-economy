182054d5daab1c40b3bed21f640b46bc686d8b44
Merel Jacobs <merel.burleigh@gmail.com>
2025-03-22 14:59:15 -0700

    test

--- client/src/pages/SearchByState.jsx
+++ client/src/pages/SearchByState.jsx
@@ -39,7 +39,7 @@ function SearchByState() {
         try {
             const data = await getProductAverages();
             console.log("Fetched Kroger Data:", data);
-            console.log(data.filter(d => d.record_day.includes("2024-03-22")));
+            console.log('march 22', data.filter(d => d.record_day.includes("2025-03-22")));
             setProductAverages(data);
             setLoading(false)
         } catch (error) {
