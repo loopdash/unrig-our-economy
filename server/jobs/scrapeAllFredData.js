5ed791848be93bae3e7df474b815327950a0ed26
Merel Jacobs <merel.burleigh@gmail.com>
2026-04-10 08:07:47 -0700

    adds other scrapes back

--- server/jobs/scrapeAllFredData.js
+++ server/jobs/scrapeAllFredData.js
@@ -5,17 +5,19 @@ const path = require("path");
 const API_KEY = "08b47ceb3cb0ccb30ce164d11c0603c6";
 const BASE_URL = "https://api.stlouisfed.org/fred/series/observations";
 
+// run with node server/jobs/scrapeAllFredData.js
+
 // Path to save the SQL file
 const sqlFilePath = path.join(__dirname, "fred_sql.sql");
 
 // Array of series IDs and corresponding filenames
 // https://fred.stlouisfed.org/series/seriesID
 const series = [
-  //   { id: 'APU0000708111', name: 'egg' },      // Eggs
-  //   { id: 'APU0000703112', name: 'beef' },    // Beef
-  //   { id: 'APU0000709112', name: 'milk' },     // Milk
-  //   { id: 'APU0000702111', name: 'bread' },    // Bread
-  //   { id: 'APU0000717311', name: 'coffee' },   // Coffee
+    { id: 'APU0000708111', name: 'egg' },      // Eggs
+    { id: 'APU0000703112', name: 'beef' },    // Beef
+    { id: 'APU0000709112', name: 'milk' },     // Milk
+    { id: 'APU0000702111', name: 'bread' },    // Bread
+    { id: 'APU0000717311', name: 'coffee' },   // Coffee
   //   add new series below
   { id: "APU0000FC3101", name: "steaks" }, // Steaks
   { id: "APU0000FD4101", name: "pork" }, // Pork
